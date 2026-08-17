25dbab04418ece8eacc367bd350cae51b9c94542
Guillerme Dalgobbo Fagundes <guillermefagundes4@gmail.com>
2026-08-17 15:41:56 -0300

    Adiciona 10_ordenacao.py

---
+++ 10_ordenacao.py
@@ -0,0 +1,7 @@
+"""Exercício 10 - Exibir a lista em ordem crescente e decrescente."""
+
+numeros = [18, 5, 12, 3, 20, 7, 9]
+
+print("Original:", numeros)
+print("Crescente:", sorted(numeros))
+print("Decrescente:", sorted(numeros, reverse=True))
