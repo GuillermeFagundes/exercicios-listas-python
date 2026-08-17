f646359874abfc1921da4c94ae6be5e2a45d49a8
Guillerme Dalgobbo Fagundes <guillermefagundes4@gmail.com>
2026-08-17 15:42:00 -0300

    Adiciona 11_fatiamento.py

---
+++ 11_fatiamento.py
@@ -0,0 +1,8 @@
+"""Exercício 11 - Realizar operações de fatiamento (slicing)."""
+
+valores = [10, 20, 30, 40, 50, 60, 70, 80]
+
+print("Quatro primeiros:", valores[:4])
+print("Três últimos:", valores[-3:])
+print("Posições 2 até 5:", valores[2:6])
+print("Lista invertida:", valores[::-1])
